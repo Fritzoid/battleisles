b20c480d458ea0e67f3283f3f52b0d2a3b4f060c
Fritzoid <frederic.gos@gmail.com>
2023-10-27 08:45:39 +0000

    more cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,17 @@
 use std::collections::HashMap;
-use std::f32::consts::TAU;
-use bevy::{
-    prelude::*, 
-    window::PrimaryWindow, 
-    window::WindowResolution, 
-    input::mouse::MouseWheel
-};
+use bevy::prelude::*; 
+use bevy::window::WindowResolution;
+use bevy::window::PrimaryWindow;
+use bevy::render::mesh::Indices;
+use bevy::render::render_resource::PrimitiveTopology;
 use bevy_panorbit_camera::{PanOrbitCamera, PanOrbitCameraPlugin};
 use hexx::*;
-mod hexx_mesh;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             primary_window: Some(Window {
-                resolution: WindowResolution::new(600., 600.),
+                resolution: WindowResolution::new(800., 600.),
                 canvas: Some("#bevy".to_owned()),
                 ..default()
             }),
@@ -23,12 +20,11 @@ fn main() {
         .add_plugins(PanOrbitCameraPlugin)
         .add_systems(Startup, setup)
         .add_systems(Update, handle_input)
-        .add_systems(Update, handle_camera_fov)
         .run()
 }
 
 
-#[derive(Debug, Resource)]
+#[derive(Resource)]
 struct BattleMap {
     layout: HexLayout,
     entities: HashMap<Hex, Entity>,
@@ -47,49 +43,20 @@ fn setup(
     let layout = HexLayout { hex_size: Vec2::splat(20.0),  ..default()};
     let default_material = materials.add(Color::BLUE.into());
     let selected_material = materials.add(Color::RED.into());
-    let hexx_mesh: Mesh = hexx_mesh::hexx_mesh(&layout);
+    let hexx_mesh: Mesh = hexx_plane(&layout);
     let mesh = meshes.add(hexx_mesh);
 
-    let projection = Projection::Perspective(PerspectiveProjection { fov: fov(600.,600., std::f32::consts::PI/4.0), ..default()});
-
-    let cam: Camera3dBundle = Camera3dBundle {
-        projection: projection,
-        ..default()
-    };
-
     commands.spawn((
-        // Note we're setting the initial position below with alpha, beta, and radius, hence
-        // we don't set transform on the camera.
-        cam,
+        Camera3dBundle::default(),
         MyGameCamera,
         PanOrbitCamera {
-            // Set focal point (what the camera should look at)
             focus: Vec3::new(0.0, 0.0, 0.0),
-            // Set the starting position, relative to focus (overrides camera's transform).
-//            alpha: Some(TAU / 8.0),
-//            beta: Some(TAU / 8.0),
             alpha: Some(0.),
             beta: Some(0.),
             radius: Some(200.0),
-            // Set limits on rotation and zoom
-//            alpha_upper_limit: Some(TAU / 4.0),
-//            alpha_lower_limit: Some(-TAU / 4.0),
-//            beta_upper_limit: Some(TAU / 3.0),
-//            beta_lower_limit: Some(-TAU / 3.0),
-//            zoom_upper_limit: Some(5.0),
-//            zoom_lower_limit: Some(1.0),
-            // Adjust sensitivity of controls
             orbit_sensitivity: 1.5,
             pan_sensitivity: 0.5,
             zoom_sensitivity: 0.5,
-            // Allow the camera to go upside down
-//            allow_upside_down: true,
-            // Change the controls (these match Blender)
-//            button_orbit: MouseButton::Middle,
-//            button_pan: MouseButton::Middle,
-//            modifier_pan: Some(KeyCode::ShiftLeft),
-            // Reverse the zoom direction
-//            reversed_zoom: true,
             ..default()
         },
     ));
@@ -117,20 +84,23 @@ fn setup(
     });
 }
 
-fn fov(height: f32, newheight: f32, fov: f32 ) -> f32 {
-    ((fov / 2.0).tan() * (newheight/height)).atan() * 2.0
+fn hexx_plane(hex_layout: &HexLayout) -> Mesh {
+    let mesh_info: MeshInfo = PlaneMeshBuilder::new(hex_layout).facing(Vec3::Z).build();
+    let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
+    mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, mesh_info.vertices);
+    mesh.insert_attribute(Mesh::ATTRIBUTE_NORMAL, mesh_info.normals);
+    mesh.insert_attribute(Mesh::ATTRIBUTE_UV_0, mesh_info.uvs);
+    mesh.set_indices(Some(Indices::U16(mesh_info.indices)));
+    mesh
 }
-
 
 fn handle_input(
     mut commands: Commands,
     windows: Query<&Window, With<PrimaryWindow>>,
     cameras: Query<(&Camera, &GlobalTransform)>,
     buttons: Res<Input<MouseButton>>,
-    mut scroll: EventReader<MouseWheel>,
     map: Res<BattleMap>,
 ) {
-    /*
     let window = windows.single();
     let (camera, cam_transform) = cameras.single();
     if let Some(pos) = window
@@ -139,28 +109,18 @@ fn handle_input(
     {
         if buttons.just_pressed(MouseButton::Left) {
             let coord = map.layout.world_pos_to_hex(pos);
+            for e in map.entities.iter()
+            {
+                commands
+                    .entity(*e.1)
+                    .insert(map.default_material.clone());
+            }
+
             if let Some(entity) = map.entities.get(&coord).copied() {
                 commands
                     .entity(entity)
                     .insert(map.selected_material.clone());
             }
         }
-
-        use bevy::input::mouse::MouseScrollUnit;
-        for ev in scroll.iter() {
-            match ev.unit {
-                MouseScrollUnit::Line => {
-                }
-                MouseScrollUnit::Pixel => {
-                    println!("Scroll (pixel units): vertical: {}, horizontal: {}", ev.y, ev.x);
-                }
-            }
-        }
     }
-     */
-}
-
-fn handle_camera_fov(mut q: Query<&mut Projection, With<MyGameCamera>>) {
-    let mut projection = q.single_mut();
-    dbg!(projection);
 }
