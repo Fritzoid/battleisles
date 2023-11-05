b9df55989c8f6b4d0a4d1e8889697e7e308ee29e
Frederic Gos <frederic.gos@gmail.com>
2023-11-05 21:22:41 +0100

    more battlemap

--- src/battle_map.rs
+++ src/battle_map.rs
@@ -1,70 +1,39 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Debug)]
-enum Type {
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+enum HexType {
     Ocean,
     Water,
     Plain,
     Hill,
-    Mountain
+    Mountain,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Hex {
-    hex_type: Type,
+    hex_type: HexType,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct BattleMap {
     size: (u16,u16), 
-    hexes: Vec<Hex>
+    hexes: Vec<Hex>,
 }
 
 impl BattleMap {
 
-    pub fn new() -> BattleMap {
-        let mut map = BattleMap {
-            size: (3,3),
-            hexes: Vec::new(),
-        };
-
-        map.hexes.push(Hex {
-            hex_type: Type::Ocean,
-        });
-        map.hexes.push(Hex {
-            hex_type: Type::Plain,
-        });
-        map.hexes.push(Hex {
-            hex_type: Type::Plain,
-        });
-        map.hexes.push(Hex {
-            hex_type: Type::Ocean,
-        });
-        map.hexes.push(Hex {
-            hex_type: Type::Plain,
-        });
-        map.hexes.push(Hex {
-            hex_type: Type::Plain,
-        });
-        map.hexes.push(Hex {
-            hex_type: Type::Ocean,
-        });
-        map.hexes.push(Hex {
-            hex_type: Type::Plain,
-        });
-        map.hexes.push(Hex {
-            hex_type: Type::Plain,
-        });
-        map
-    }
-
     pub fn from_json(input: &str) -> BattleMap {
-        let battle_map = serde_json::from_str(input).unwrap();
-        battle_map
+        match serde_json::from_str(input) {
+            Ok(battle_map) => battle_map,
+            Err(e) => panic!("Failed to deserialize battle map: {}", e)
+        }
     }
 
     pub fn to_json(&self) -> String {
-        serde_json::to_string(self).unwrap()
+        match serde_json::to_string(&self) {
+            Ok(json) => json,
+            Err(e) => panic!("Failed to serialize battle map: {}", e),
+        }
     }
 }
 
@@ -92,5 +61,30 @@ mod tests {
         let map = BattleMap::from_json(mapstr);
         assert_eq!(map.size, (3,3));
         assert_eq!(map.hexes.len(), 9);
+        assert_eq!(map.hexes[0].hex_type, HexType::Ocean);
+        assert_eq!(map.hexes[3].hex_type, HexType::Mountain);
     }
+
+    #[test]
+    fn to_json_test() {
+        let map = BattleMap {
+            size: (1, 3),
+            hexes: vec![
+                Hex {
+                    hex_type: HexType::Ocean
+                },
+                Hex {
+                    hex_type: HexType::Plain
+                },
+                Hex {
+                    hex_type: HexType::Water
+                },
+            ]
+        };
+
+        let json = r#"{"size":[1,3],"hexes":[{"hex_type":"Ocean"},{"hex_type":"Plain"},{"hex_type":"Water"}]}"#;
+        let output = map.to_json();
+        assert_eq!(output, json);
+    }
+
 }
