7009ccba78a84a866845212d802fb0a42f8cda5a
Frederic Gos <frederic.gos@gmail.com>
2024-11-30 11:20:34 +0100

    Refactor water material initialization to use ExtendedMaterial for deep and shallow water

--- crates/battleisles_game/src/map.rs
+++ crates/battleisles_game/src/map.rs
@@ -38,12 +38,6 @@ pub fn init_map(
     let shape = RegularPolygon::new(HEX_SIZE, 6);
     let mesh = Extrusion::new(shape, HEX_THICKNESS);
     let mesh_handle = meshes.add(mesh);
-    let deep_water_mat = materials.add(StandardMaterial::from_color(
-        bevy::color::palettes::css::DARK_BLUE,
-    ));
-    let shallow_water_mat = materials.add(StandardMaterial::from_color(
-        bevy::color::palettes::css::BLUE,
-    ));
     let plains_mat = materials.add(StandardMaterial::from_color(
         bevy::color::palettes::css::GREEN,
     ));
@@ -54,9 +48,42 @@ pub fn init_map(
         bevy::color::palettes::css::YELLOW,
     ));
 
-    let fancy_water = water_materials.add(ExtendedMaterial {
+    let deepwater_mat = water_materials.add(ExtendedMaterial {
         base: StandardMaterial {
-            base_color: bevy::color::palettes::css::BLACK.into(),
+            base_color: bevy::color::palettes::css::DARK_BLUE.into(),
+            perceptual_roughness: 0.0,
+            ..default()
+        },
+        extension: Water {
+            normals: asset_server.load_with_settings::<Image, ImageLoaderSettings>(
+                "textures/water_normals.png",
+                |settings| {
+                    settings.is_srgb = false;
+                    settings.sampler = ImageSampler::Descriptor(ImageSamplerDescriptor {
+                        address_mode_u: ImageAddressMode::Repeat,
+                        address_mode_v: ImageAddressMode::Repeat,
+                        mag_filter: ImageFilterMode::Linear,
+                        min_filter: ImageFilterMode::Linear,
+                        ..default()
+                    });
+                },
+            ),
+            // These water settings are just random values to create some
+            // variety.
+            settings: WaterSettings {
+                octave_vectors: [
+                    vec4(0.080, 0.059, 0.073, -0.062),
+                    vec4(0.153, 0.138, -0.149, -0.195),
+                ],
+                octave_scales: vec4(1.0, 2.1, 7.9, 14.9) * 5.0,
+                octave_strengths: vec4(0.16, 0.18, 0.093, 0.044),
+            },
+        },
+    });
+
+    let shallowwater_mat = water_materials.add(ExtendedMaterial {
+        base: StandardMaterial {
+            base_color: bevy::color::palettes::css::BLUE.into(),
             perceptual_roughness: 0.0,
             ..default()
         },
@@ -94,11 +121,22 @@ pub fn init_map(
     for h in 1..=map.height {
         x = 0.0;
         for w in 1..=map.width {
-
             if map.hexes[i] == HexType::DeepWater {
                 commands.spawn(MaterialMeshBundle {
                     mesh: mesh_handle.clone(),
-                    material: fancy_water.clone(),
+                    material: deepwater_mat.clone(),
+                    transform: Transform {
+                        translation: Vec3::new(x, 0.0, z),
+                        rotation: Quat::from_rotation_x(-std::f32::consts::FRAC_PI_2)
+                            * Quat::from_rotation_z(std::f32::consts::FRAC_PI_2 / 3.0),
+                        ..default()
+                    },
+                    ..default()
+                });
+            } else if map.hexes[i] == HexType::ShallowWater {
+                commands.spawn(MaterialMeshBundle {
+                    mesh: mesh_handle.clone(),
+                    material: shallowwater_mat.clone(),
                     transform: Transform {
                         translation: Vec3::new(x, 0.0, z),
                         rotation: Quat::from_rotation_x(-std::f32::consts::FRAC_PI_2)
@@ -112,11 +150,10 @@ pub fn init_map(
                 commands.spawn(PbrBundle {
                     mesh: mesh_handle.clone(),
                     material: match map.hexes[i] {
-                        HexType::DeepWater => deep_water_mat.clone(),
-                        HexType::ShallowWater => shallow_water_mat.clone(),
                         HexType::Plains => plains_mat.clone(),
                         HexType::Mountains => mountains_mat.clone(),
                         HexType::Hills => hills_mat.clone(),
+                        _ => unreachable!(),
                     },
                     transform: Transform {
                         translation: Vec3::new(x, 0.0, z),
