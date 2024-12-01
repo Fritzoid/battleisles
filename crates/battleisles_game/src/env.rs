83efb635a6f882e52ef5adc343e2c55931f796d8
Frederic Gos <frederic.gos@gmail.com>
2024-12-01 10:23:02 +0100

    Refactor environment initialization by updating camera position and commenting out unused light and skybox code

--- crates/battleisles_game/src/env.rs
+++ crates/battleisles_game/src/env.rs
@@ -8,6 +8,7 @@ use bevy::pbr::ScreenSpaceReflectionsSettings;
 struct GameCamera;
 
 pub fn init_env(commands: &mut Commands, asset_server: &Res<AssetServer>) {
+/* 
     commands.spawn(PointLightBundle {
         point_light: PointLight {
             shadows_enabled: true,
@@ -16,11 +17,11 @@ pub fn init_env(commands: &mut Commands, asset_server: &Res<AssetServer>) {
         transform: Transform::from_xyz(4.0, 8.0, 4.0),
         ..default()
     });
-
+*/
     commands.spawn((
         Camera3dBundle {
-            transform: Transform::from_translation(Vec3::new(20.0, 20.0, 30.0)) // Set initial position
-                .looking_at(Vec3::ZERO, Vec3::Y), // Make the camera look at the origin
+            transform: Transform::from_translation(Vec3::new(0.0, 70.0, 0.0)) // Set initial position
+                .looking_at(Vec3::ZERO, -Vec3::Z), // Make the camera look at the origin
             ..default()
         },
         GameCamera,
@@ -30,10 +31,12 @@ pub fn init_env(commands: &mut Commands, asset_server: &Res<AssetServer>) {
         specular_map: asset_server.load("environment_maps/pisa_specular_rgb9e5_zstd.ktx2"),
         intensity: 5000.0,
     })
+/* 
      .insert(Skybox {
         image: asset_server.load("environment_maps/pisa_specular_rgb9e5_zstd.ktx2"),
         brightness: 5000.0,
     })
+*/
     .insert(ScreenSpaceReflectionsBundle::default())
     .insert(ScreenSpaceReflectionsSettings::default())
     .insert(Fxaa::default());
